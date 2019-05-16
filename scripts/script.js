b9606e6202e6d9d85ed98f4be04e870c1098ad57
CKeogh <c.keogh.10.ck@gmail.com>
2019-05-16 16:26:53 +0100

    add animated banner

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@
 const navBar = document.querySelector('.navbar');
 
 const handleScroll = (event) => {
-    if (window.scrollY > 530 && ![...navBar.classList].includes('scrolledStyle')) {
+    if (window.scrollY > 630 && ![...navBar.classList].includes('scrolledStyle')) {
         navBar.classList.remove('fadeInDown')
         navBar.classList.add('scrolledStyle')
         navBar.classList.add('slideInDown')
-    } else if (window.scrollY < 530 && [...navBar.classList].includes('scrolledStyle')) {
+    } else if (window.scrollY < 630 && [...navBar.classList].includes('scrolledStyle')) {
         navBar.classList.remove('scrolledStyle');
         navBar.classList.remove('slideInDown');
     }
